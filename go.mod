039581c451dd129bb21c0d18bea7151045255980
Gurjeet Singh <gurjeet@singh.im>
2022-02-10 11:04:08 -0800

    Used glog and demonstrated the problem

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/glog_fix_test
 
 go 1.16
+
+require github.com/golang/glog v1.0.0
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+
+	"github.com/golang/glog"
+)
+
+type Level int32
+
+// Ensure that Level implements the interface flag.Value
+var _ flag.Value = (*Level)(nil)
+
+var verbosity Level
+
+// Dummy implementations
+func (l *Level) Get() interface{}     { return nil }
+func (l *Level) Set(val string) error { return nil }
+func (l *Level) String() string       { return "" }
+
+func init() {
+	/*
+	 * Trying to declare an option 'v' causes a Panic at runtime, because the
+	 * glog package has already registered that, and a few other options, in its
+	 * init() method.
+	 */
+	flag.Var(&verbosity, "v", "Emit verbose execution progress")
+}
 
 func main() {
-	fmt.Println("vim-go")
+	// Parse the command-line options
+	flag.Parse()
+
+	glog.V(1).Info("Hello from V(1).Info()")
 }
